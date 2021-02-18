02cab9f03a61c9b60c56a8ba34bd770880a12300
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-02-18 04:51:35 +0200

    Add reusable React components

---
+++ components/UI.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Centered = ({
+	children,
+}: {
+	children: React.ReactNode
+}) => <div className='flex-grow flex items-center mb-10 justify-center'>
+	<div className='sm:flex gap-x-5 max-w-2xl mx-5'>
+		{children}
+	</div>
+</div>;
