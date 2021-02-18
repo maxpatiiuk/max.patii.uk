2d9fe02cc309cc8bf0ea564fafeac2de877f0975
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-02-18 21:21:16 +0200

    Finish shop deprecation notice message

--- components/ErrorPage.tsx
+++ components/ErrorPage.tsx
@@ -19,26 +19,24 @@ const languageStrings: LanguageStringsStructure<{
 const ErrorPage = ({errorCode = 404}: {errorCode?: number}) =>
 	<Layout title={errorCode.toString()}>
 		{(language) =>
-			<>
-				<Centered>
-					<div className="text-center">
-						<h1 className='text-9xl py-2 text-indigo-300'>{
-							errorCode
-						}</h1>
-						<h2>{languageStrings[language].header}</h2>
-						<p>
-							{languageStrings[language].message}
-							<Link href="/">
-								<a className={`block pt-10 transition
-									text-red-400 hover:text-black`}>
-									{commonStrings[
-										language].returnToHomePage}
-								</a>
-							</Link>
-						</p>
-					</div>
-				</Centered>
-			</>
+			<Centered>
+				<div className="text-center">
+					<h1 className='text-9xl py-2 text-indigo-300'>{
+						errorCode
+					}</h1>
+					<h2>{languageStrings[language].header}</h2>
+					<p>
+						{languageStrings[language].message}
+						<Link href="/">
+							<a className={`block pt-10 transition
+								text-red-400 hover:text-black`}>
+								{commonStrings[
+									language].returnToHomePage}
+							</a>
+						</Link>
+					</p>
+				</div>
+			</Centered>
 		}
 	</Layout>;
 
--- components/UI.tsx
+++ components/UI.tsx
@@ -4,8 +4,7 @@ export const Centered = ({
 	children,
 }: {
 	children: React.ReactNode
-}) => <div className='flex-grow flex items-center mb-10 justify-center'>
-	<div className='sm:flex gap-x-5 max-w-2xl mx-5'>
-		{children}
-	</div>
+}) => <div className='w-screen h-screen flex items-center justify-center
+	text-center'>
+	{children}
 </div>;
