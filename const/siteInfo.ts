c460f4a80cd0846245bf3301c1bb4be0875bddc0
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-02-18 04:51:05 +0200

    Add configuration options

---
+++ const/siteInfo.ts
@@ -0,0 +1,26 @@
+import { LanguageStringsStructure } from '../lib/languages';
+import { strip }                    from '../lib/localizationHelper';
+
+const siteInfo: LanguageStringsStructure<{
+	title: string,
+	description: string,
+	keywords: string,
+	author: string,
+	yes: string,
+	no: string,
+}> = {
+	'en-US': {
+		title: 'MAMBO.IN.UA',
+		description: strip(`MAMBO.IN.UA - Maksym Patiiuk's personal
+			blog`),
+		keywords: strip(`Maksym Patiiuk, Maksym Patiiuk blog,
+			Maksym Patiiuk CV, Max Patiiuk, Maksym Patiiuk portfolio
+			mambo shop, mambo, В гостях у MAMBO, мамбо,
+			mambo experimental, Максим Патіюк`),
+		author: 'Maksym Patiiuk',
+		yes: 'Yes',
+		no: 'No'
+	},
+};
+
+export default siteInfo;
---
+++ localization/global.ts
@@ -0,0 +1,9 @@
+import { LanguageStringsStructure } from '../lib/languages';
+
+export const commonStrings: LanguageStringsStructure<{
+	returnToHomePage: string,
+}> = {
+	'en-US': {
+		returnToHomePage: '← Return to homepage',
+	},
+};
