1aa2aa0f1c2984d88d51f8aca8e1be07f2e92378
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-04-19 03:09:57 +0300

    Replace Steam's link with GitHub
    Why haven't I done this earlier?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   purge: {
-    content: [
-      './pages/**/*.tsx',
-      './components/**/*.tsx',
-    ],
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
     options: {
       keyframes: true,
     },
@@ -13,47 +10,47 @@ module.exports = {
     clear: false,
     skew: false,
   },
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
     extend: {
       colors: {
         facebook: {
-          dark: '#359',
-          light: '#468',
+          dark: "#359",
+          light: "#468",
         },
         twitter: {
-          dark: '#3ae',
-          light: '#4bf',
+          dark: "#3ae",
+          light: "#4bf",
         },
         instagram: {
-          dark: '#c38',
-          light: '#d48',
+          dark: "#c38",
+          light: "#d48",
         },
         email: {
-          dark: '#83b',
-          light: '#94c',
+          dark: "#83b",
+          light: "#94c",
         },
         linked_in: {
-          dark: '#47b',
-          light: '#58c',
+          dark: "#47b",
+          light: "#58c",
         },
-        steam: {
-          dark: '#222',
-          light: '#333',
+        github: {
+          dark: "#222",
+          light: "#333",
         },
       },
     },
   },
   variants: {
-    transitionProperty: ['responsive', 'motion-safe', 'motion-reduce'],
-    animation: ['responsive', 'motion-safe', 'motion-reduce'],
-    borderWidth: ['responsive', 'hover', 'focus'],
+    transitionProperty: ["responsive", "motion-safe", "motion-reduce"],
+    animation: ["responsive", "motion-safe", "motion-reduce"],
+    borderWidth: ["responsive", "hover", "focus"],
     extend: {
-      padding: ['hover'],
-      backgroundImage: ['hover'],
-      fontSize: ['group-hover'],
-      backgroundColor: ['disabled'],
-      cursor: ['disabled']
+      padding: ["hover"],
+      backgroundImage: ["hover"],
+      fontSize: ["group-hover"],
+      backgroundColor: ["disabled"],
+      cursor: ["disabled"],
     },
   },
   plugins: [],
