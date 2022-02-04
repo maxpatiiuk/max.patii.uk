a63615eb6050f9fb2abb545e8b17d14df682e42c
Max Patiiuk <max@patii.uk>
2022-02-04 10:03:15 -0600

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-  purge: {
-    content: ['./pages/**/*.tsx', './components/**/*.tsx'],
-    options: {
-      keyframes: true,
-    },
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  options: {
+    keyframes: true,
   },
   corePlugins: {
     float: false,
     clear: false,
     skew: false,
   },
-  darkMode: 'media',
   theme: {
     extend: {
       colors: {
@@ -41,17 +38,4 @@ module.exports = {
       },
     },
   },
-  variants: {
-    transitionProperty: ['responsive', 'motion-safe', 'motion-reduce'],
-    animation: ['responsive', 'motion-safe', 'motion-reduce'],
-    borderWidth: ['responsive', 'hover', 'focus'],
-    extend: {
-      padding: ['hover'],
-      backgroundImage: ['hover'],
-      fontSize: ['group-hover'],
-      backgroundColor: ['disabled', 'active'],
-      cursor: ['disabled'],
-    },
-  },
-  plugins: [],
 };
