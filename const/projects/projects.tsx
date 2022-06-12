4e5c88563280b41a2ee9b931bdacc2f5887d62a1
Max Patiiuk <max@patii.uk>
2022-06-12 11:07:47 -0500

    Add a post about my portfolio

--- const/projects/projects.tsx
+++ const/projects/projects.tsx
@@ -5,6 +5,7 @@
  *  - Type safety included (since posts are defined in TypeScript)
  *  - Autocompletion & Grammar checker included (thanks to IDE plugins)
  *  - Easy to search among all posts at once
+ *  - All content is version controlled
  *  - Since there aren't too many posts, and they aren't modified often, and
  *    there aren't multiple people editing them, and there aren't multiple
  *    localizations of a post, this is definitelly the simplest way to get the
@@ -58,6 +59,9 @@ import workbench1 from '../../public/projects/images/workbench/1.png';
 import workbench2 from '../../public/projects/images/workbench/2.png';
 import workbench3 from '../../public/projects/images/workbench/3.png';
 import workbench4 from '../../public/projects/images/workbench/4.jpg';
+import portfolio1 from '../../public/projects/images/portfolio/1.jpeg';
+import portfolio2 from '../../public/projects/images/portfolio/2.jpeg';
+import portfolio3 from '../../public/projects/images/portfolio/3.jpeg';
 import { internshipProjects } from './internshipProjects';
 
 export type Project = {
@@ -554,6 +558,58 @@ export const projects: IR<Project> = {
               </Link>
               .
             </Paragraph>
+          </>
+        ),
+      },
+    },
+  },
+  portfolio: {
+    gitHub: 'https://github.com/maxxxxxdlp/max.patii.uk/',
+    localized: {
+      'en-US': {
+        title: 'Portfolio',
+        description: 'A minimalistic Next.js application',
+        content: (
+          <>
+            <Paragraph>
+              When it came to designing my portfolio, I wanted to make it
+              represent my aspirations for simplicity and purpose. That is why
+              there are no cluttered submenus, no useless footers, and no
+              unnecessary information.
+            </Paragraph>
+
+            <Paragraph>
+              This site is primarily used as my portfolio, though it can also
+              host random JS projects (
+              <Link href="https://max.patii.uk/projects/tetris">
+                check out my Tetris Game
+              </Link>
+              ).
+            </Paragraph>
+
+            <Header>Screenshots</Header>
+            <Image source={portfolio1}>Main page</Image>
+            <Image source={portfolio2}>
+              A post about "Specify 7 WorkBench"
+            </Image>
+
+            <Header>Technologies used</Header>
+            <List>
+              <li>Javascript ES6+</li>
+              <li>TypeScript</li>
+              <li>React</li>
+              <li>Next.js</li>
+              <li>Tailwind.CSS</li>
+            </List>
+
+            <Header>Credits</Header>
+            <Paragraph>
+              The design was inspired by{' '}
+              <Link href="https://clementgrellier.fr/">
+                Clement Grellier's portfolio.
+              </Link>
+            </Paragraph>
+            <Image source={portfolio3}>An inspiration for the design</Image>
           </>
         ),
       },
