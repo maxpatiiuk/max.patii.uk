34f87713a7e5431ebb6ecd0eb6e8b31efee2bfe1
Max Patiiuk <max@patii.uk>
2022-08-22 18:25:00 -0500

    Add a return to homepage link

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,6 +1,7 @@
 import type { GetStaticPaths } from 'next';
 import Head from 'next/head';
 import React from 'react';
+import { icons } from '../../components/icons';
 
 import Layout from '../../components/Layout';
 import { projects } from '../../const/projects/projects';
@@ -12,10 +13,12 @@ type ProjectIdQuery = {
 };
 
 const languageStrings: LanguageStringsStructure<{
-  viewOnGitHub: string;
+  readonly viewOnGitHub: string;
+  readonly returnToHomePage: string;
 }> = {
   'en-US': {
     viewOnGitHub: 'View this project on GitHub',
+    returnToHomePage: 'Return to Home Page',
   },
 };
 
@@ -72,19 +75,28 @@ export default function ProjectPage({ id }: ProjectIdQuery): JSX.Element {
               </>
             )}
           </Head>
-          <header className="flex items-end justify-center h-48 bg-gray-800">
-            <div className="gap-y-3 max-w-[1000px] flex flex-col p-4 text-center">
-              <h1 className="w-full text-3xl">
-                {project.localized[language].title}
-              </h1>
-              {typeof project.gitHub === 'string' && (
-                <a
-                  className="hover:text-white w-full text-gray-300"
-                  href={project.gitHub}
-                >
-                  {languageStrings[language].viewOnGitHub}
-                </a>
-              )}
+          <header className="flex h-48 bg-gray-800 justify-center">
+            <div className="gap-y-3 w-full max-w-[1000px] flex flex-col p-4 md:px-0 justify-center">
+              <a
+                className="hover:text-white w-full text-gray-300 flex"
+                href="/"
+              >
+                {icons.chevronLeft}
+                {languageStrings[language].returnToHomePage}
+              </a>
+              <div className="flex-1 flex flex-col justify-center text-center">
+                <h1 className="w-full text-3xl">
+                  {project.localized[language].title}
+                </h1>
+                {typeof project.gitHub === 'string' && (
+                  <a
+                    className="hover:text-white w-full text-gray-300"
+                    href={project.gitHub}
+                  >
+                    {languageStrings[language].viewOnGitHub}
+                  </a>
+                )}
+              </div>
             </div>
           </header>
           <main className="flex justify-center p-4 pt-16 pb-16">
