adfc773c0de3181ad87dc77343a1e820a80cccb2
Max Patiiuk <max@patii.uk>
2024-01-15 20:05:07 -0800

    refactor(postcss): simplify configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,6 @@
 module.exports = {
-  plugins:
-    // Process.env.NODE_ENV === 'production' ?
-    [
-      'tailwindcss',
-      'postcss-flexbugs-fixes',
-      [
-        'postcss-preset-env',
-        {
-          autoprefixer: {
-            flexbox: 'no-2009',
-          },
-          stage: 3,
-          features: {
-            'custom-properties': false,
-          },
-        },
-      ],
-    ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
